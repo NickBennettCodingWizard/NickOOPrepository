8aa86ac0b94ea0f4db152f8cee3086bd1ed5860e
RusselTheWonderMuscle <nick.bennett.911@gmail.com>
2020-01-21 15:35:22 -0500

    putting together project

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,19 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
 
 /**
  *
  * @author nickb
  */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        org.eclipse.jetty.start.Main.main(args);
     }
-    
 }
